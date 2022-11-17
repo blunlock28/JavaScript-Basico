9894d41d335f82239dad802c42d9b0727bce11d5
blunlock28 <blunlock28@gmail.com>
2022-11-16 22:51:48 -0400

    ejercicios Number

---
+++ sesion-05-numbers/ejercicio_5.js
@@ -0,0 +1,15 @@
+// - Una variable que contenga tu altura en centímetros (entero)
+let altura = 185
+// - Una variable que contenga tu altura en metros (número de coma flotante)
+let alturaMetros = 1.85
+// - Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let pesoKilogramo = 95.99
+// - Una variable que contenga tu altura en metros redondeada hacia arriba
+let alturaRedondeoUp =1.85
+console.log(Math.ceil(alturaRedondeoUp)+"mts")
+// - Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let pesoRedondeoDown = 95.99
+console.log(Math.floor(pesoRedondeoDown)+"kg")
+// - Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+valor_max=Number.MAX_VALUE + 1 === Number.MAX_VALUE
+console.log(valor_max)
